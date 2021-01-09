61a989c50c36ed56afbf4a473449b3fc9e6fbad8
Chris Knight <ChrisK@beckhoff.com>
2021-01-09 23:15:05 +0000

    fixed bugs in listview display, add binding to listview for tasklist, added rremove option by passing command paramter

--- WpfToDoList/WpfToDoList/MainWindow.xaml.cs
+++ WpfToDoList/WpfToDoList/MainWindow.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Collections.ObjectModel;
+﻿using System.IO;
 using System.Windows;
 using System.Text.Json;
 using System.Windows.Data;
-using System.ComponentModel;
-using System.Windows.Input;
 
 namespace WpfToDoList
 {
@@ -29,95 +25,6 @@ namespace WpfToDoList
             //dataFilePath = Path.Combine(paths);
         }
 
-        //private void addTask_Click(object sender, RoutedEventArgs e)
-        //{
-        //    ToDoListItem toDoListItem = new ToDoListItem();
-        //    toDoListItem.Name = taskNameField.Text;
-        //    toDoListItem.Description = taskDescriptionField.Text;
-        //    mainList.Add(toDoListItem);
-        //}
-
-        //private void removeTask_Click(object sender, RoutedEventArgs e)
-        //{
-        //    mainList.RemoveAt(taskList.SelectedIndex);
-        //}
-
-        //private void save_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string jsonString = JsonSerializer.Serialize(mainList);
-        //    File.WriteAllText(dataFilePath, jsonString);
-        //}
-
-        //private void load_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string jsonString = File.ReadAllText(dataFilePath);
-        //    ObservableCollection<ToDoListItem> loaded = JsonSerializer.Deserialize<ObservableCollection<ToDoListItem>>(jsonString);
-        //    foreach (ToDoListItem x in loaded)
-        //    {
-        //        mainList.Add(x);
-        //    }
-        //    MessageBox.Show(mainList.Count.ToString());
-        //}
-    }
-
-    public class MainView : INotifyPropertyChanged
-    {
-        
-
-        private ToDoListItem _newTask = new ToDoListItem();
-
-        public ToDoListItem NewTask
-        {
-            get
-            {
-                return _newTask;
-            }
-            set
-            {
-                _newTask = value;
-                OnPropertyChanged("NewTask");
-            }
-        }
-
-        private ObservableCollection<ToDoListItem> toDoListItems = new ObservableCollection<ToDoListItem>();
-
-        public ICommand AddTask { get; set; }
-        public ICommand RemoveTask { get; set; }
-
-        public MainView()
-        {
-            AddTask = new Command(AddTaskMethod, canAddTask);
-            RemoveTask = new Command(RemoveTaskMethod, canRemoveTask);
-        }
-
-        private void AddTaskMethod(Object paramter)
-        {
-            MessageBox.Show("YaY");
-        }
-
-        private bool canAddTask(Object paramter)
-        {
-            return true;
-        }
-
-        private void RemoveTaskMethod(Object parameter)
-        {
-            MessageBox.Show("removed");
-        }
-
-        private bool canRemoveTask(Object parameter)
-        {
-            return true;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
-            }
-        }
+       
     }
 }
---
+++ WpfToDoList/WpfToDoList/ViewModel/MainView.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfToDoList
+{
+    public class MainView : INotifyPropertyChanged
+    {
+        private ToDoListItem _newTask = new ToDoListItem();
+
+        public ToDoListItem NewTask
+        {
+            get
+            {
+                return _newTask;
+            }
+            set
+            {
+                _newTask = value;
+                OnPropertyChanged("NewTask");
+            }
+        }
+
+        private ObservableCollection<ToDoListItem> _taskList = new ObservableCollection<ToDoListItem>();
+
+        public ObservableCollection<ToDoListItem> TaskList
+        {
+            get
+            {
+                return _taskList;
+            }
+            set
+            {
+                _taskList = value;
+                OnPropertyChanged("TaskList");
+            }
+        }
+
+        public ICommand AddTask { get; set; }
+        public ICommand RemoveTask { get; set; }
+
+        public MainView()
+        {
+            AddTask = new Command(AddTaskMethod, canAddTask);
+            RemoveTask = new Command(RemoveTaskMethod, canRemoveTask);
+        }
+
+        private void AddTaskMethod(Object paramter)
+        {
+            TaskList.Add(_newTask);
+        }
+
+        private bool canAddTask(Object paramter)
+        {
+            return true;
+        }
+
+        private void RemoveTaskMethod(Object parameter)
+        {
+            MessageBox.Show("remove");
+        }
+
+        private bool canRemoveTask(Object parameter)
+        {
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+    //private void addTask_Click(object sender, RoutedEventArgs e)
+    //{
+    //    ToDoListItem toDoListItem = new ToDoListItem();
+    //    toDoListItem.Name = taskNameField.Text;
+    //    toDoListItem.Description = taskDescriptionField.Text;
+    //    mainList.Add(toDoListItem);
+    //}
+
+    //private void removeTask_Click(object sender, RoutedEventArgs e)
+    //{
+    //    mainList.RemoveAt(taskList.SelectedIndex);
+    //}
+
+    //private void save_Click(object sender, RoutedEventArgs e)
+    //{
+    //    string jsonString = JsonSerializer.Serialize(mainList);
+    //    File.WriteAllText(dataFilePath, jsonString);
+    //}
+
+    //private void load_Click(object sender, RoutedEventArgs e)
+    //{
+    //    string jsonString = File.ReadAllText(dataFilePath);
+    //    ObservableCollection<ToDoListItem> loaded = JsonSerializer.Deserialize<ObservableCollection<ToDoListItem>>(jsonString);
+    //    foreach (ToDoListItem x in loaded)
+    //    {
+    //        mainList.Add(x);
+    //    }
+    //    MessageBox.Show(mainList.Count.ToString());
+    //}
+}
